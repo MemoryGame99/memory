624a6884df0475b82a2c3b315dab5b46d578939f
KamilO <wheels4@o2.pl>
2020-09-20 11:42:34 +0200

    commit przed mergem do mastera z drobnymi zmianami w klasach

--- src/main/java/pl/project/memorygame/engine/Card.java
+++ src/main/java/pl/project/memorygame/engine/Card.java
@@ -3,13 +3,19 @@ package pl.project.memorygame.engine;
 import lombok.Getter;
 
 @Getter
-public class CardPicture <T extends Pictures> {
+public class Card<T extends Pictures> {
 
     private T picture;
+
     int value;
 
-    public CardPicture(T picture, int value) {
+    public Card(T picture, int value) {
         this.picture = picture;
         this.value = value;
+    }
+
+    private enum cardSide{
+        OBVERSE,
+        REVERSE;
     }
 }
